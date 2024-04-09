4c178e39a55fb9f199ab6474555ba4ceb045d11a
Aytuğ HAN <me@aytughan.com>
2024-04-09 03:08:55 +0300

    OG Route Fix

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -3,17 +3,6 @@ import { ImageResponse } from 'next/og';
 export const runtime = 'edge';
 
 export async function GET(request: Request, response: Response) {
-  const interSemiBold = fetch(
-    new URL(
-      'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap'
-    )
-  ).then((res) => res.arrayBuffer());
-
-  const size = {
-    width: 1200,
-    height: 630,
-  };
-
   try {
     const { searchParams } = new URL(request.url);
 
@@ -37,21 +26,7 @@ export async function GET(request: Request, response: Response) {
         >
           {title}
         </div>
-      ),
-      // ImageResponse options
-      {
-        // For convenience, we can re-use the exported opengraph-image
-        // size config to also set the ImageResponse's width and height.
-        ...size,
-        fonts: [
-          {
-            name: 'Inter',
-            data: await interSemiBold,
-            style: 'normal',
-            weight: 400,
-          },
-        ],
-      }
+      )
     );
   } catch (e: any) {
     return new Response('Failed to generate OG image', { status: 500 });
